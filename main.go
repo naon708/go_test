cea25498aa56127a5442f542138754491440e421
naon708 <naoya_nis@icloud.com>
2022-06-22 22:30:59 +0900

    Add: Bravo!

--- main.go
+++ main.go
@@ -5,7 +5,13 @@ import "fmt"
 func main() {
     fmt.Println("Hello, Go")
     howMuch("Alice", 100)
+
+    fmt.Println(bravo())
 }
 func howMuch(name string, money int) {
-    fmt.Printf("%s has %d yen", name, money)
+    fmt.Printf("%s has %d yen\n", name, money)
+}
+
+func bravo() string {
+    return "Bravo!"
 }
